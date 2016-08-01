78d968769087f88f129c1607bbe58d2ab8158143
yugq <yugq@fenqi.im>
2016-08-01 12:57:39 +0800

    remove TestDockerCmdExecFactory

--- src/main/java/com/github/dockerjava/test/TestClient.java
+++ src/main/java/com/github/dockerjava/test/TestClient.java
@@ -4,21 +4,12 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Info;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.core.TestDockerCmdExecFactory;
 
 public class TestClient {
 	
-    protected static TestDockerCmdExecFactory dockerCmdExecFactory = initTestDockerCmdExecFactory();
-    protected static TestDockerCmdExecFactory initTestDockerCmdExecFactory() {
-        return new TestDockerCmdExecFactory(
-                DockerClientBuilder.getDefaultDockerCmdExecFactory());
-    }
-
 	public static void main(String[] args){
 //		DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://localhost:2375").build();
-		DockerClient dockerClient = DockerClientBuilder.getInstance(config())
-                .withDockerCmdExecFactory(dockerCmdExecFactory)
-                .build();
+		DockerClient dockerClient = DockerClientBuilder.getInstance(config()).build();
 		Info info = dockerClient.infoCmd().exec();
 		System.out.print(info);
 		System.out.println("Hello docker!");
