106f4c95fd94796f70f531d305fdc264167dc8c6
yugq <yugq@fenqi.im>
2016-08-01 13:07:43 +0800

    resolve DOCKER_HOST problem by revising docker.service on CentOS

--- src/main/java/com/github/dockerjava/test/TestClient.java
+++ src/main/java/com/github/dockerjava/test/TestClient.java
@@ -2,30 +2,14 @@ package com.github.dockerjava.test;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Info;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 
 public class TestClient {
 	
 	public static void main(String[] args){
-//		DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://localhost:2375").build();
-		DockerClient dockerClient = DockerClientBuilder.getInstance(config()).build();
+		DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://localhost:2375").build();
 		Info info = dockerClient.infoCmd().exec();
 		System.out.print(info);
 		System.out.println("Hello docker!");
 	}
-	
-    private static DefaultDockerClientConfig config() {
-        return config(null);
-    }
-    
-    protected static DefaultDockerClientConfig config(String password) {
-        DefaultDockerClientConfig.Builder builder = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                .withRegistryUrl("https://index.docker.io/v1/");
-        if (password != null) {
-            builder = builder.withRegistryPassword(password);
-        }
-
-        return builder.build();
-    }
 }
