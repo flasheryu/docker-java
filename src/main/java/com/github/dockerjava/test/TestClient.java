01774c95cbae64700defdcfda64ad1a0f24479e3
yugq <yugq@fenqi.im>
2016-08-01 11:40:29 +0800

    add test class

---
+++ src/main/java/com/github/dockerjava/test/TestClient.java
@@ -0,0 +1,14 @@
+package com.github.dockerjava.test;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Info;
+import com.github.dockerjava.core.DockerClientBuilder;
+
+public class TestClient {
+
+	public static void main(String[] args){
+		DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://localhost:2375").build();
+		Info info = dockerClient.infoCmd().exec();
+		System.out.print(info);
+	}
+}
