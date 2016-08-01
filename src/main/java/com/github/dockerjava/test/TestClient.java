e637d5b44115df34ed18846f26148a7355b78400
yugq <yugq@fenqi.im>
2016-08-01 12:35:31 +0800

    fix bug: DOCKER_HOST cannot be connected

--- src/main/java/com/github/dockerjava/test/TestClient.java
+++ src/main/java/com/github/dockerjava/test/TestClient.java
@@ -2,14 +2,39 @@ package com.github.dockerjava.test;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Info;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.TestDockerCmdExecFactory;
 
 public class TestClient {
+	
+    protected static TestDockerCmdExecFactory dockerCmdExecFactory = initTestDockerCmdExecFactory();
+    protected static TestDockerCmdExecFactory initTestDockerCmdExecFactory() {
+        return new TestDockerCmdExecFactory(
+                DockerClientBuilder.getDefaultDockerCmdExecFactory());
+    }
 
 	public static void main(String[] args){
-		DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://localhost:2375").build();
+//		DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://localhost:2375").build();
+		DockerClient dockerClient = DockerClientBuilder.getInstance(config())
+                .withDockerCmdExecFactory(dockerCmdExecFactory)
+                .build();
 		Info info = dockerClient.infoCmd().exec();
 		System.out.print(info);
 		System.out.println("Hello docker!");
 	}
+	
+    private static DefaultDockerClientConfig config() {
+        return config(null);
+    }
+    
+    protected static DefaultDockerClientConfig config(String password) {
+        DefaultDockerClientConfig.Builder builder = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withRegistryUrl("https://index.docker.io/v1/");
+        if (password != null) {
+            builder = builder.withRegistryPassword(password);
+        }
+
+        return builder.build();
+    }
 }
